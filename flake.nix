d1604b6696893bc4bb38b6f8dd88d1ef7401b0e4
DocRatMD <47792236+DocRatMD@users.noreply.github.com>
2026-03-14 06:40:51 +0000

    chore(update): helium to 0.10.4.1

--- flake.nix
+++ flake.nix
@@ -26,11 +26,11 @@
         helium = pkgs.appimageTools.wrapType2 rec {
 
           pname = "helium";
-          version = "0.10.2.1";
+          version = "0.10.4.1";
 
           src = pkgs.fetchurl {
             url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
-            sha256 = "sha256-Kh6UgdleK+L+G4LNiQL2DkQIwS43cyzX+Jo6K0/fX1M=";
+            sha256 = "sha256-JjNtf5UoGIQ8fkHqsWAERmKRLc3FKIr11fnoRhuZCSQ=";
           };
 
           extraInstallCommands =
